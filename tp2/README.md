38e0b8d36f6d9ac0797ea62dad9e54e5221b4f26
Stéphane NGUYEN <stephane.nguyen@etu.unige.ch>
2024-09-30 13:47:27 +0200

    adding tp2

---
+++ tp2/README.md
@@ -0,0 +1,80 @@
+# [11X001] TD n°2
+
+---
+
+Université de Genève, cours [11X001 Introduction à la Programmation des Algorithmes](https://wwwi.unige.ch/cursus/programme-des-cours/web/teachings/details/2021-11X001).
+
+---
+
+## Rappel: scanf
+
+Un programme peut lire un entier en entrée comme ceci:
+
+```c
+int mon_entier;
+scanf("%d", &mon_entier);
+```
+
+et une chaîne de caractères comme cela:
+
+```c
+char ma_chaine[30];
+scanf("%s", ma_chaine);
+```
+
+---
+
+## Exercice 1 
+
+**TODO** : Lire un entier relatif et afficher s'il est strictement négatif, nul ou strictement positif.
+
+---
+
+## Exercice 2
+
+**TODO**: Lire un entier naturel et afficher la somme des entiers naturels jusqu'à ce dernier (inclus) à l'aide d'une boucle `while`.  
+**TODO**: Même consigne mais cette fois-ci une boucle `for` doit être utilisée.
+
+---
+
+## Exercice 3
+
+**TODO** : Lire un entier compris entre 0 et 6 et afficher le jour de la semaine correspondant à l'aide d'un `if` / ` else if` / `else`.  
+**TODO** : Même consigne mais cette fois-ci un `switch` doit être utilisé.
+
+---
+
+## Exercice 4
+
+**TODO** : Lire 5 entiers, les stocker dans un tableau puis afficher la somme des éléments du tableau.  
+**TODO** : Afficher le plus petit élément et le plus grand élément contenus dans ce tableau.
+
+---
+
+## Exercice 5
+
+**TODO** : Lire le prénom et la note d'un étudiant d'IPA puis stocker ces informations dans une structure. Enfin, afficher un message indiquant si l'étudiant a validé la matière.  
+**TODO** : Cette fois-ci les informations de 3 étudiants sont lues et stockées dans un tableau d'étudiants. Le tableau est ensuite parcouru pour indiquer si chaque étudiant a validé la matière.  
+**TODO** : A l'aide de `sizeof`, afficher le nombre d'éléments contenus par le tableau d'étudiants.
+
+---
+
+## Exercice 6
+Une compagnie aérienne souhaite appeler les passagers de ses différents vols par leurs prénoms et numéros de sièges:
+- Un vol est composé d'un point de départ, d'une destination, d'une durée de vol et d'un tableau de passagers.
+- Chaque passager est composé de son prénom et d'un numéro de siège.
+
+**TODO** : 
+
+- Déclarer les différentes structures nécessaires à la réalisation de cette tâche.
+- Lire en entrée les informations de 3 passagers de 2 vols différents et créer les structures correspondantes puis stocker les vols dans un tableau.
+- Pour chaque vol, afficher les informations du vol et appeler ses passagers.
+
+---
+
+## Exercice 7
+
+**TODO** : Afficher un sapin de Noël (2 triangles de 10 lignes et 1 rectangle de 6 lignes) à l'aide de boucles `for` et des caractères `' ', '*'` et `'\n'`.  
+**TODO** : Lire les dimensions voulues du sapin, les stocker dans un tableau puis répéter l'étape précédente.
+
+
---
+++ tp2/main_empty.c
@@ -0,0 +1,165 @@
+#include <math.h>
+#include <stdio.h>
+
+
+void exercice1(void) {
+    printf("\n\nEXERCICE 1\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice2(void) {
+    printf("\n\nEXERCICE 2\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice2_bis(void) {
+    printf("\n\nEXERCICE 2 BIS\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice3(void) {
+    printf("\n\nEXERCICE 3\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice3_bis(void) {
+    printf("\n\nEXERCICE 3 BIS\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice4(void) {
+    printf("\n\nEXERCICE 4\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice4_bis(void) {
+    printf("\n\nEXERCICE 4 BIS\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice5(void) {
+    printf("\n\nEXERCICE 5\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice5_bis(void) {
+    printf("\n\nEXERCICE 5 BIS\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice5_ter(void) {
+    printf("\n\nEXERCICE 5 TER\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+}
+
+
+void exercice6(void) {
+    printf("\n\nEXERCICE 6\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice7(void) {
+    printf("\n\nEXERCICE 7\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+void exercice7_bis(void) {
+    printf("\n\nEXERCICE 7 BIS\n\n");
+    
+    /******************** Votre code ci-dessous ********************/
+
+    /******************** Votre code ci-dessus *********************/
+
+    return;
+}
+
+
+int main(void) {   
+
+    // Astuce : commenter tous les exercices sauf celui en cours pour gagner du temps !
+    
+    exercice1();
+    exercice2();
+    exercice2_bis();
+    exercice3();
+    exercice3_bis();
+    exercice4();
+    exercice4_bis();
+    exercice5();
+    exercice5_bis();
+    exercice5_ter();
+    exercice6();
+    exercice7();
+    exercice7_bis();
+
+    return 0;
+}
